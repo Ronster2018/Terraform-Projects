374887f3e44a18e021a4141fde5630e4a5662018
K'Ron <simmons.kron@gmail.com>
2023-09-17 12:24:53 -0400

    Added ECR, VPC, ECS, ELB, and ECS infra

--- ECR_ECS_VPC_ELB/1_ecr.tf
+++ ECR_ECS_VPC_ELB/1_ecr.tf
@@ -3,7 +3,9 @@ resource "aws_ecr_repository" "flask_app" {
   name = "flask-app"
 }
 
-# aws ecr get-login-password --region REGION | docker login --username AWS --password-stdin ID.dkr.ecr.REGION.amazonaws.com
+#Linux: aws ecr get-login-password --region REGION | docker login --username AWS --password-stdin ID.dkr.ecr.REGION.amazonaws.com
+
+# Windows: (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin ID.dkr.ecr.REGION.amazonaws.com
 # docker build -t flask-app .
 # docker tag flask-app:latest ID.dkr.REGION.amazonaws.com/flask-app:latest
 # docker push ID.dkr.REGION.amazonaws.com/flask-app:latest
